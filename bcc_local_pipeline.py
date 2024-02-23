48d42e3f225833bf7398b8664abf8bfe13def911
Niko Febrianur <nikofebrianur@gmail.com>
2024-02-23 09:20:35 +0800

    fix pipeline input value

--- bcc_local_pipeline.py
+++ bcc_local_pipeline.py
@@ -10,13 +10,13 @@ PIPELINE_NAME = "bank_customer_churn_pipeline"
 
 # pipeline inputs
 DATA_ROOT = "data"
-TRANSFORM_MODULE_FILE = "modules/transform.py"
-TRAINER_MODULE_FILE = "modules/trainer.py"
+TRANSFORM_MODULE_FILE = "modules/bcc_transform.py"
+TRAINER_MODULE_FILE = "modules/bcc_trainer.py"
 # requirement_file = os.path.join(root, "requirements.txt")
 
 # pipeline outputs
 OUTPUT_BASE = "output"
-serving_model_dir = os.path.join(OUTPUT_BASE, 'serving_model')
+serving_model_dir = os.path.join(OUTPUT_BASE, "serving_model")
 pipeline_root = os.path.join(OUTPUT_BASE, PIPELINE_NAME)
 metadata_path = os.path.join(pipeline_root, "metadata.sqlite")
 
